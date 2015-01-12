18bbabbfa756f6ff33e4faddca00462c32358754
Ross Hettel <rhettel@highground.com>
2015-01-11 22:27:59 -0600

    change require on mocha test

--- test/_setup.js
+++ test/_setup.js
@@ -2,7 +2,7 @@
 global.assert = require('assert');  //set assert to global so we don't have to keep requiring it
 
 before(function (done) {
-    var NounProject = require('../nounProject.js'),
+    var NounProject = require('../nounproject.js'),
         nounProject = new NounProject({
             key: '8cc6316e5a93414aa72470379f48a368',
             secret: '087b4f12335849e0adf84907db086f4f'
